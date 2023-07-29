2ae42b8d100f81ed0561d69334bad6dadcd5ca25
roman_developer <89508006+RomanDeveloperGit@users.noreply.github.com>
2023-07-29 17:51:52 +0300

    Add question

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,6 +2,7 @@ import { type Component } from 'solid-js';
 
 import { NoteListPage } from '../pages/note-list-page';
 
+// Добавить собачку на импорт
 // можно JSX засунуть в сигнал?
 
 export const App: Component = () => {
