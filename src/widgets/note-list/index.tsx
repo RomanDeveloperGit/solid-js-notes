8d6258af42a90d1e368a4a661b2134c1ade94ef2
roman_developer <89508006+RomanDeveloperGit@users.noreply.github.com>
2023-07-30 11:36:08 +0300

    Fix note list

--- src/widgets/note-list/index.tsx
+++ src/widgets/note-list/index.tsx
@@ -1,4 +1,4 @@
-import { For } from 'solid-js';
+import { For, Show } from 'solid-js';
 
 import { PageKey } from '../../shared/constants/pages';
 import { getRedirectPageHandler } from '../../shared/lib/get-redirect-page-handler';
@@ -13,36 +13,36 @@ import styles from './styles.module.css';
 
 export const NoteList = () => {
   return (
-    <div class={styles.container}>
-      {noteModel.getFilteredNoteList().length ||
-      noteModel.getNoteListSearchText() ? (
-        <>
-          <div class={styles.filters}>
-            <SearchBox />
-            <SortBox />
-          </div>
-          <div class={styles.list}>
-            {/* При поиске fallback остается навсегда, если ничего не находится. Исправить */}
-            <For
-              each={noteModel.getFilteredNoteList()}
-              fallback={<div>Загрузка...</div>}
-            >
-              {(item) => (
-                <NoteCard
-                  deleteButtonSlot={<DeleteNoteButton id={item.id} />}
-                  handleRedirectToDetailsNotePage={getRedirectPageHandler(
-                    PageKey.NoteDetails,
-                    item.id,
-                  )}
-                  {...item}
-                />
-              )}
-            </For>
-          </div>
-        </>
-      ) : (
-        <div>У вас нет заметок</div>
-      )}
-    </div>
+    <Show
+      when={noteModel.getNoteList().length}
+      fallback={<div>У вас нет заметок</div>}
+    >
+      <div class={styles.filters}>
+        <SearchBox />
+        <SortBox />
+      </div>
+      <div class={styles.list}>
+        <Show
+          when={noteModel.getFilteredNoteList().length}
+          fallback={<div>Ничего не найдено</div>}
+        >
+          <For
+            each={noteModel.getFilteredNoteList()}
+            fallback={<div>Загрузка...</div>}
+          >
+            {(item) => (
+              <NoteCard
+                deleteButtonSlot={<DeleteNoteButton id={item.id} />}
+                handleRedirectToDetailsNotePage={getRedirectPageHandler(
+                  PageKey.NoteDetails,
+                  item.id,
+                )}
+                {...item}
+              />
+            )}
+          </For>
+        </Show>
+      </div>
+    </Show>
   );
 };
